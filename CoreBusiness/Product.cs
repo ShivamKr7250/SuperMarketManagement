17f43f12469fd37c78c63dfb7d5dbd9fe8e5ee6d
Shivam <supshiv7250@gmail.com>
2023-03-26 23:27:34 +0530

    AddProduct use case Completed

--- CoreBusiness/Product.cs
+++ CoreBusiness/Product.cs
@@ -12,16 +12,16 @@ namespace CoreBusiness
         public int ProductId { get; set; }
 
         [Required]
-        public int CategoryId { get; set; }
+        public int? CategoryId { get; set; }
 
         [Required]
         public string? Name { get; set; }
 
         [Required]
-        public int Quantity { get; set; }
+        public int? Quantity { get; set; }
 
         [Required]
-        public double Price { get; set; }
+        public double? Price { get; set; }
 
     }
 }
